2ba7b5d0fcb0cf5193adbe9b37db79452c1d0730
Nalinshankar <nalinmakaut@gmail.com>
2018-12-27 15:57:14 +0530

    1st added

---
+++ src/main/java/com/stackroute/javape3/StudentMarks.java
@@ -0,0 +1,13 @@
+package com.stackroute.javape3;
+
+public class StudentMarks {
+    private int noOfStudents;   //take it from user
+
+    public int [] stuGrades;  //take it from user
+
+    public String checkForGrades(int no_of_students , int[] grades){
+        //write your own logic
+        return null;
+    }
+
+}
